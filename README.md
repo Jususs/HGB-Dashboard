1843a5db55a22fe58b103c80c4a9a4a8683a99b5
Jususs <j.warm@gmx.at>
2025-12-05 12:18:32 +0100

    basic weather implementation

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # HGB-Dashboard
+
+A dashboard giving an overview over the current lunch meals and weather conditions to make life a bit easier.
--- dashboard/src/app/app.component.ts
+++ dashboard/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,6 +8,15 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'dashboard';
+  weather: any
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.http.get("https://api.open-meteo.com/v1/forecast?latitude=48.3679&longitude=14.5169&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current=temperature_2m,apparent_temperature&timezone=Europe%2FBerlin&forecast_days=1").subscribe(response => {
+      this.weather = response;
+    });
+  }
 }
