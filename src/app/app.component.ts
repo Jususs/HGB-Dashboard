d686e01a98540c3407bff79a040dd99419fcd5ae
Jususs <j.warm@gmx.at>
2025-12-05 16:30:59 +0100

    added date pipe and weather condition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,38 @@
 import {AfterViewInit, Component, OnInit, Renderer2} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {weatherCodes} from './weatherCodes';
+import {DatePipe} from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [
+    DatePipe
+  ],
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit, AfterViewInit {
   title = 'dashboard';
-  weather: any
-  currentTemperature: any
-  apparentTemperature: any
-  dailyMaxTemperature: any
-  dailyMinTemperature: any
-  sunrise: any
-  sunset: any
+  weatherReport: any;
+  weather: any;
+  currentTemperature: any;
+  dailyMaxTemperature: any;
+  dailyMinTemperature: any;
+  sunrise: any;
+  sunset: any;
 
   constructor(private http: HttpClient, private renderer: Renderer2) {}
 
   ngOnInit() {
-    this.http.get("https://api.open-meteo.com/v1/forecast?latitude=48.3679&longitude=14.5169&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current=temperature_2m,apparent_temperature&timezone=Europe%2FBerlin&forecast_days=1").subscribe(response => {
-      this.weather = response;
-      this.currentTemperature = this.weather.current.temperature_2m;
-      this.apparentTemperature = this.weather.current.apparent_temperature;
-      this.dailyMaxTemperature = this.weather.daily.temperature_2m_max[0];
-      this.dailyMinTemperature = this.weather.daily.temperature_2m_min[0];
-      this.sunrise = this.weather.daily.sunrise[0];
-      this.sunset = this.weather.daily.sunset[0];
+    this.http.get("https://api.open-meteo.com/v1/forecast?latitude=48.3679&longitude=14.5169&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,rain_sum,snowfall_sum&current=temperature_2m,weather_code&timezone=Europe%2FBerlin&forecast_days=1").subscribe(response => {
+      this.weatherReport = response;
+
+      this.weather = weatherCodes[this.weatherReport.current.weather_code].name;
+      this.currentTemperature = this.weatherReport.current.temperature_2m;
+      this.dailyMaxTemperature = this.weatherReport.daily.temperature_2m_max[0];
+      this.dailyMinTemperature = this.weatherReport.daily.temperature_2m_min[0];
+      this.sunrise = this.weatherReport.daily.sunrise[0];
+      this.sunset = this.weatherReport.daily.sunset[0];
     });
   }
 
---
+++ src/app/weatherCodes.ts
@@ -0,0 +1,40 @@
+export const weatherCodes = [
+    {id: 0, name: "Clear sky"},
+    {id: 1, name: "Mainly clear"},
+    {id: 2, name: "Partly cloudy"},
+    {id: 3, name: "Overcast"},
+
+    {id: 45, name: "Fog"},
+    {id: 48, name: "Rime fog"},
+
+    {id: 51, name: "Light drizzle"},
+    {id: 53, name: "Moderate drizzle"},
+    {id: 54, name: "Dense Drizzle"},
+
+    {id: 56, name: "Light freezing drizzle"},
+    {id: 57, name: "Dense freezing drizzle"},
+
+    {id: 61, name: "Slight rain"},
+    {id: 63, name: "Moderate rain"},
+    {id: 65, name: "Heavy rain"},
+
+    {id: 66, name: "Light freezing rain"},
+    {id: 67, name: "Heavy freezing rain"},
+
+    {id: 71, name: "Slight snow fall"},
+    {id: 73, name: "Moderate snow fall"},
+    {id: 75, name: "Heavy snow fall"},
+
+    {id: 77, name: "Snow grains"},
+
+    {id: 80, name: "Slight rain showers"},
+    {id: 81, name: "Moderate rain showers"},
+    {id: 82, name: "Heavy rain showers"},
+
+    {id: 85, name: "Slight snow shower"},
+    {id: 86, name: "Heavy snow showers"},
+
+    {id: 95, name: "Thunderstorm"},
+    {id: 96, name: "Thunderstorm with slight hail"},
+    {id: 99, name: "Thunderstorm with heavy hail"},
+]
